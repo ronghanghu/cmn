8b0a58c9990481a477d0cb40a7675c5497dd0c6a
Ronghang Hu <ronghang.hu@gmail.com>
2018-03-13 23:14:04 -0700

    Add all_qa_pairs_relationship.json

--- exp-visual7w/data/download_imdb.sh
+++ exp-visual7w/data/download_imdb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ./exp-visual7w/data
 wget http://people.eecs.berkeley.edu/~ronghang/projects/cmn/data/exp-visual7w/imdb.tgz
+wget http://people.eecs.berkeley.edu/~ronghang/projects/cmn/data/exp-visual7w/all_qa_pairs_relationship.json
 tar xvf imdb.tgz
 cd ../..
